0e6a70b577552d9aec70e2f8fa184c2e860535fd
Pouya Sadeghimirani <pooya@arianalabs.io>
2025-04-11 00:01:41 +0330

    feat: add storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { useState } from "react";
 import "./App.css";
 import AppProviders from "./providers/AppProviders";
+import { Button } from "./components/ui/button";
+import { useState } from "react";
 
 function App() {
-  const [sortBy, setSortBy] = useState("latest");
-  return <AppProviders>salam</AppProviders>;
+  const [count, setCount] = useState(0);
+  return (
+    <AppProviders>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <h1>{count}</h1>
+        <Button onClick={() => setCount((prev) => prev + 1)}>Click me</Button>
+      </div>
+    </AppProviders>
+  );
 }
 
 export default App;
